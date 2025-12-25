264f940399b60a6e54df8acd1c20e44500000cc8
httpmarco <httpmarco@gmx.de>
2025-12-25 11:28:18 +0100

    Update and fix dependency next 16 mistakes

--- next.config.mjs
+++ next.config.mjs
@@ -1,62 +1,71 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['localhost', 'mineskin.eu'],
-    unoptimized: false,
-  },
-  
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-  
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  trailingSlash: false,
-  poweredByHeader: false,
-  
-  generateBuildId: async () => {
-    return 'build-' + Date.now();
-  },
-  
-  async rewrites() {
-    return [
-      {
-        source: '/login',
-        destination: '/login',
-        has: [
-          {
-            type: 'header',
-            key: 'x-vercel-deployment',
-          },
+    output: 'standalone',
+    turbopack: {},
+    images: {
+        unoptimized: false,
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'mineskin.eu',
+                pathname: '/**',
+            },
         ],
-      },
-      {
-        source: '/onboarding',
-        destination: '/onboarding',
-        has: [
-          {
-            type: 'header',
-            key: 'x-vercel-deployment',
-          },
-        ],
-      },
-    ];
-  },
+    },
+
+    webpack: (config, {isServer}) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+                net: false,
+                tls: false,
+            };
+        }
+        return config;
+    },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+
+    trailingSlash: false,
+    poweredByHeader: false,
+
+    generateBuildId: async () => {
+        return 'build-' + Date.now();
+    },
+
+    async rewrites() {
+        return [
+            {
+                source: '/login',
+                destination: '/login',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'x-vercel-deployment',
+                    },
+                ],
+            },
+            {
+                source: '/onboarding',
+                destination: '/onboarding',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'x-vercel-deployment',
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
